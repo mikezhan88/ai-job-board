0acfd6c7f59b40975076dda2ce06010ed3c21cec
Mike Zhan <mikezhan8@gmail.com>
2025-06-23 22:57:56 -0700

    Add resume processing function and update job listing page

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -11,10 +11,6 @@ export const env = createEnv({
     CLERK_SECRET_KEY: z.string().min(1),
     CLERK_WEBHOOK_SECRET: z.string().min(1),
     UPLOADTHING_TOKEN: z.string().min(1),
-    ANTHROPIC_API_KEY: z.string().min(1),
-    GEMINI_API_KEY: z.string().min(1),
-    RESEND_API_KEY: z.string().min(1),
-    SERVER_URL: z.string().min(1),
   },
   createFinalSchema: env => {
     return z.object(env).transform(val => {
