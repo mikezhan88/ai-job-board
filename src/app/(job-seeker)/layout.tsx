d27f30c17e2c8f1265caba029d987749b72aa578
Mike Zhan <mikezhan8@gmail.com>
2025-06-23 17:34:48 -0700

    Add job listings pages and UI components

--- src/app/(job-seeker)/layout.tsx
+++ src/app/(job-seeker)/layout.tsx
@@ -1,35 +1,54 @@
-import { ReactNode } from "react"
-import {AppSidebar} from "@/components/sidebar/AppSidebar"
-import { LogInIcon, ClipboardListIcon, BrainCircuitIcon, LayoutDashboard } from "lucide-react"
-import { SidebarUserButton } from "@/features/users/components/SidebarUserButton"
+import { AppSidebar } from "@/components/sidebar/AppSidebar"
 import { SidebarNavMenuGroup } from "@/components/sidebar/SidebarNavMenuGroup"
+import { SidebarUserButton } from "@/features/users/components/SidebarUserButton"
+import {
+  BrainCircuitIcon,
+  ClipboardListIcon,
+  LayoutDashboard,
+  LogInIcon,
+} from "lucide-react"
+import { ReactNode } from "react"
 
-
-export default function JobSeekerLayout({children}: {children: ReactNode}) {
-    return <AppSidebar content={ 
-        <SidebarNavMenuGroup className="mt-auto" items={[
-            { href: "/", icon: <ClipboardListIcon />, label: "Job Board" },
-            {
-              href: "/ai-search",
-              icon: <BrainCircuitIcon />,
-              label: "AI Search",
-            },
-            {
-              href: "/employer",
-              icon: <LayoutDashboard />,
-              label: "Employer Dashboard",
-              authStatus: "signedIn",
-            },
-            {
-              href: "/sign-in",
-              icon: <LogInIcon />,
-              label: "Sign In",
-              authStatus: "signedOut",
-            },
-          ]} >
-            
-        </SidebarNavMenuGroup>
-    } footerButton={<SidebarUserButton />}>
-  {children}
-  </AppSidebar>
+export default function JobSeekerLayout({
+  children,
+  sidebar,
+}: {
+  children: ReactNode
+  sidebar: ReactNode
+}) {
+  return (
+    <AppSidebar
+      content={
+        <>
+          {sidebar}
+          <SidebarNavMenuGroup
+            className="mt-auto"
+            items={[
+              { href: "/", icon: <ClipboardListIcon />, label: "Job Board" },
+              {
+                href: "/ai-search",
+                icon: <BrainCircuitIcon />,
+                label: "AI Search",
+              },
+              {
+                href: "/employer",
+                icon: <LayoutDashboard />,
+                label: "Employer Dashboard",
+                authStatus: "signedIn",
+              },
+              {
+                href: "/sign-in",
+                icon: <LogInIcon />,
+                label: "Sign In",
+                authStatus: "signedOut",
+              },
+            ]}
+          />
+        </>
+      }
+      footerButton={<SidebarUserButton />}
+    >
+      {children}
+    </AppSidebar>
+  )
 }
